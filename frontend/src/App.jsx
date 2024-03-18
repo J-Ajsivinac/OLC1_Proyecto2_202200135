361eb0ba8f6ee29e279a8fe27efbaf45a7d07ba1
J-Ajsivinac <3114791110409@ingenieria.usac.edu.gt>
2024-03-17 23:45:59 -0600

    Agrega configuraciones de Tailwind CSSy Vite para el frontend.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="relative inline-block text-left">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
