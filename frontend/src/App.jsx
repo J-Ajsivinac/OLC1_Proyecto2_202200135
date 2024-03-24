24ecdc808442aa4a49061402ad6154ac9b5f31b7
J-Ajsivinac <3114791110409@ingenieria.usac.edu.gt>
2024-03-24 17:49:57 -0600

    Agregado de estilos de desplazamiento y manejo de errores en la consola

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,12 @@ import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night'
 import { PiWarningCircleFill } from "react-icons/pi";
 import { FaChevronDown } from "react-icons/fa6";
 import './theme.css'
+import './scroll.css'
 
 function App() {
   // const [count, setCount] = useState(0)
   const [value, setValue] = useState("console.log('hello world!');");
+  const error = "---> Este es un error de ejemplo  \n---> Error No. 2";
   const [isOpen, setIsOpen] = useState(false);
   const onChange = useCallback((val, _viewUpdate) => {
     console.log('val:', val);
@@ -24,32 +26,30 @@ function App() {
           <div className="w-3/5">
             <div className="flex flex-col w-full rounded-md bg-panel-dark h-full overflow-auto px-4">
               <span className="text-[#bdbec0] pt-3 pb-5 font-bold">Código</span>
-              <CodeMirror value={value} height="560px" onChange={onChange} extensions={cpp()} theme={tokyoNight} aria-autocomplete={true} />
+              <CodeMirror value={value} height="555px" onChange={onChange} extensions={cpp()} theme={tokyoNight} aria-autocomplete={true} />
+              {/* <span className="text-text-gray w-full text-end">Joab Ajsivinac - 202200135</span> */}
             </div>
           </div>
           <div className="w-2/5">
             <div className="flex flex-col w-full rounded-md bg-panel-dark h-full px-4">
               <span className="text-[#bdbec0] pt-3 pb-5 font-bold">Consola</span>
-              <div className="w-full h-full flex flex-col mb-2">
-                <textarea name="" id="" className="w-full h-full bg-panel-dark text-[#adb6c4] outline-none resize-none"></textarea>
+              <div className="w-full h-full flex flex-col mb-2 gap-y-4">
+                <textarea name="" id="" className="w-full h-full bg-panel-dark text-[#adb6c4] outline-none resize-none" readOnly></textarea>
 
                 <div
                   className={`group flex flex-col gap-2 rounded-lg bg-panel-dark border-2 border-gray-200/10 p-5 text-white ${isOpen ? 'group-focus:-translate-y-5' : ''}`}
                   tabIndex="1"
                 >
                   <div className="flex cursor-pointer items-center justify-between" onClick={() => setIsOpen(!isOpen)}>
-                    <span className="flex flex-row items-center gap-x-2 text-[#f47961]"><PiWarningCircleFill size={20} /> Errores </span>
+                    <span className="flex flex-row items-center gap-x-2 text-[#f86f6f]"><PiWarningCircleFill size={20} /> Errores </span>
                     <div className={`h-2 w-3 transition-all duration-500 ${isOpen ? 'rotate-180' : ''}`}>
                       <FaChevronDown size={12} color="#f47961" />
                     </div>
                   </div>
                   <div
-                    className={`items-center  duration-1000 transition-all ${isOpen ? 'visible max-h-screen opacity-100 h-28 overflow-auto' : 'overflow-hidden invisible h-0 max-h-0 opacity-0'}`}
+                    className={`items-center text-text-gray  duration-1000 transition-all ${isOpen ? 'visible max-h-screen opacity-100 h-28 overflow-auto' : 'overflow-hidden invisible h-0 max-h-0 opacity-0'}`}
                   >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.
+                    <pre>{error}</pre>
                   </div>
                 </div>
 
